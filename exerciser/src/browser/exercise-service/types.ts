d0ade26e8dfcffd818e0a496254ee7ef8a6c898f
Atahan Kılıç <atahan.kilic@hotmail.com>
2025-01-29 11:44:02 +0100

    linting

--- exerciser/src/browser/exercise-service/types.ts
+++ exerciser/src/browser/exercise-service/types.ts
@@ -3,17 +3,13 @@ export interface ExerciseFile{
     content: string;
 }
 
-
-
 export interface ExerciseOverview {
     exerciseId: string;
     exerciseName: string;
     exerciseSummarization: string;
-
 }
 
 export  interface Exercise extends ExerciseOverview{
-    
     fileListSummarization: string;
     exerciseFiles: ExerciseFile[];
     conductorFiles: ExerciseFile[];
